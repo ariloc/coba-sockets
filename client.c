2b99bc4fdea08eb91de5951318f11625f01ab988
ManuRC2 <manuelcolusso@live.com.ar>
2022-04-05 13:53:14 -0300

    login funcionando a medias parte 2

--- client.c
+++ client.c
@@ -7,28 +7,28 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 
 // ./clFtp [IP] [PORT]
 
-
-
+#define MAXSTRLEN 500
 #define MAXDATASIZE 500 
+
 //TODO: Pasar al .h
 char buf[MAXDATASIZE];
 int numbytes;
 
 int parse_response_code(char *code){
-    return  100*(code[0]-'0') + 10*(code[1]-'0') + (code[1]-'0');
+    return  100*(code[0]-'0') + 10*(code[1]-'0') + (code[2]-'0');
 }
 
 int login(int sock){
-    char *nombre_usuario, *contraseña;
+    char *nombre_usuario, *contrasena;
     
-    //Recibir solicitud de usuario "username: "
+    //olicitud de usuario "username: "
     printf("username: ");
-    scanf("%s", buf);
+    memset(buf, 0, sizeof(buf));
+    fgets(buf, MAXSTRLEN, stdin);
     nombre_usuario = malloc(sizeof(char)*6 + strlen(buf));
     strcat(strcat(nombre_usuario, "USER "), buf);
     nombre_usuario[strlen(nombre_usuario)] = '\0';
@@ -38,10 +38,13 @@ int login(int sock){
         return -1;
     }
     //Recibir solicitud de contraseña "password required for <nombre_usuario> "
+    memset(buf, 0, sizeof(buf));
     if((numbytes = recv(sock, buf, MAXDATASIZE, 0)) == -1){
         perror("recv");
         return -1;
     }
+    
+    printf("%s\n", buf);
 
     if(parse_response_code(buf) != 331){
         perror("response code");
@@ -49,23 +52,35 @@ int login(int sock){
     }
 
     printf("password: ");
-    scanf("%s", buf);
-    contraseña = malloc(sizeof(char)*6 + strlen(buf));
-    strcat(strcat(contraseña, "USER "), buf);
-    contraseña[strlen(contraseña)] = '\0';
+    memset(buf, 0, sizeof(buf));
+    fgets(buf, MAXSTRLEN, stdin);
+    contrasena = malloc(sizeof(char)*6 + strlen(buf));
+    strcat(strcat(contrasena, "PASS "), buf);
+    contrasena[strlen(contrasena)] = '\0';
 
     //Enviar contraseña "PASS <contraseña>"
-    if(send(sock, contraseña, strlen(contraseña)+1, 0) == -1){
+    if(send(sock, contrasena, strlen(contrasena)+1, 0) == -1){
         perror("send");
         return -1;
     }
 
+    //Recibir respuesta de user:pass
+    memset(buf, 0, sizeof(buf));
+    if((numbytes = recv(sock, buf, MAXDATASIZE, 0)) == -1){
+        perror("recv");
+        return -1;
+    }
+
     //Successful login
-    if(parse_response_code(buf) == 230)
+    if(parse_response_code(buf) == 230){
+        printf("%s", buf);
         return 1;
+        }
     //Login error
-    if(parse_response_code(buf) == 530)
+    if(parse_response_code(buf) == 530){
+        printf("%s", buf);
         return 0;
+        }
     //Unknown error
     fprintf(stderr, "Login failed, unknown error");
     return -1;
@@ -74,7 +89,7 @@ int login(int sock){
 int main (int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr,"./clFtp [IP] [PORT]\n");
-        return 1;
+        return 0;
     }
 
     struct addrinfo hints, *servinfo, *p;
--- server.c
+++ server.c
@@ -12,15 +12,31 @@
 #include <signal.h>
 
 #define BACKLOG 1 // TODO: PONER EN .h!
+#define MAXDATASIZE 500 
+
+
 
 // localhost por defecto (?)
 // ./srvFtp [PORT]
+
+char *parse_client_code(char *text){
+    char *code;
+    code = malloc(sizeof(char)*5);
+    strncpy(code, text, 4);
+    code[4] = '\0';
+    return code;
+}
+
 int main (int argc, char *argv[]) {
+    
     if (argc != 2) {
         fprintf(stderr, "./srvFtp [PORT]\n");
         return 1;
     }
 
+    char *buf;
+    buf = malloc(sizeof(char)*MAXDATASIZE);
+    int numbytes;
     struct addrinfo hints, *servinfo, *p;
     
     memset(&hints, 0, sizeof(hints));
@@ -69,7 +85,9 @@ int main (int argc, char *argv[]) {
 
     struct sockaddr_storage their_addr;
     socklen_t sin_size;
-    int new_fd;
+    int new_fd, exit;
+    char user[50], pass[50];
+
     while(1) {
         sin_size = sizeof(their_addr);
         new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
@@ -80,7 +98,67 @@ int main (int argc, char *argv[]) {
 
         if(send(new_fd, "220 srvFtp", 10, 0) == -1) 
             perror("send");
-        //else fprintf(stderr, "OKA\n");
+        else fprintf(stderr, "OKA\n");
+        exit = 1;
+        while(exit){
+            //Recibe nombre de usuario
+            printf("\nComienzo Login\n");
+            memset(buf, 0, sizeof(buf));    
+            if(numbytes = recv(new_fd, buf, MAXDATASIZE, 0) == -1){
+                perror("recv");
+                return -1;
+            }
+            //Verifica que lo recibido sea un USER
+            if(strcmp(parse_client_code(buf), "USER") == 0){
+                buf = &buf[5];
+                strcpy(user, buf);
+                user[strlen(user)-1] = '\0';
+            }
+            else{
+                fprintf(stderr, "Didn't recieve a username.");
+                return 0;
+            }
+            //Envia solicitud de contraseña al cliente
+            memset(buf, 0, sizeof(buf));
+            strcat(strcpy(buf, "331 Password required for "), user);
+            if((send(new_fd, buf, strlen(buf), 0)) == -1){
+                perror("send");
+                return -1;
+            }
+            //Recibir contraseña
+            memset(buf, 0, sizeof(buf));
+            if(numbytes = recv(new_fd, buf, MAXDATASIZE, 0) == -1){
+                perror("recv");
+                return -1;
+            }
+            //Verifica que lo recibido sea una contraseña
+            if(strcmp(parse_client_code(buf), "PASS") == 0){
+                buf = &buf[5];
+                strcpy(pass, buf);
+                pass[strlen(pass)-1] = '\0';
+            }
+            else{
+                fprintf(stderr, "Didn't recieve a password.");
+                return 0;
+            }
+            //Validar usuario y contraseña
+            if(strcmp("manu", user) == 0 && strcmp("1234", pass) == 0){
+                //login successful
+                strcat(strcat(strcpy(buf, "230 User "), user), " logged in\n");
+                if(send(new_fd, buf, strlen(buf), 0) == -1){
+                    perror("send");
+                    return -1;
+                }
+                exit = 0;
+            }
+            else{
+                //login incorrect
+                if(send(new_fd, "530 Login incorrect\n", 20, 0) == -1){
+                    perror("send");
+                    return -1;
+                }
+            }
+        }
 
         close(new_fd);
     }
